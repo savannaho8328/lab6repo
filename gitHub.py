1a726e2bfb9be7a8577051661c30fcc2f749e902
javier abadia <jabadia@ufl.edu>
2023-03-07 11:59:01 -0500

    Added decoded function in Savannah's repo

--- gitHub.py
+++ gitHub.py
@@ -1,10 +1,10 @@
-
 def print_menu():
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
+
 
 def encode(password):
     # savannah ogletree
@@ -15,6 +15,14 @@ def encode(password):
     return n_password
 
 
+def decode(n_password):
+    decoded_password = ""
+    for number in n_password:
+        if int(number) <= 2:
+            decoded_password += str((int(number) - 3) + 10)
+        else:
+            decoded_password += str(int(number) - 3)
+    return decoded_password
 
 
 if __name__ == "__main__":
@@ -23,10 +31,9 @@ if __name__ == "__main__":
         option = int(input("Please enter an option: "))
         if option == 1:
             password = input("Please enter your password to encode: ")
-            encode(password)
-            print("Your password has been encoded and stored")
+            n_password = encode(password)
+            print("Your password has been encoded and stored!")
         elif option == 2:
-            decode(n_password)
             print("The encoded password is", n_password, ", and the original password is", decode(n_password))
         else:
             break
